5c6cd55a7e1349711f0091558768a1ba7b978b27
thidtanai <thidsloth@gmail.com>
2024-08-21 17:02:44 +0700

    Delete tailwind logo

--- src/views/Toolbar.vue
+++ src/views/Toolbar.vue
@@ -55,22 +55,15 @@
         <div
           class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
         >
-          <div class="flex flex-shrink-0 items-center">
-            <img
-              class="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company"
-            />
-          </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a
+              <!-- <a
                 href="/home"
                 class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                 aria-current="page"
                 >Home</a
-              >
+              > -->
               <a
                 href="/product"
                 class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
